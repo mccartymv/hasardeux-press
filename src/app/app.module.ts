3fa9a68baec29001d556d15fbb07b3c4e5b79b72
max <mvmccart@gmail.com>
2020-10-06 16:01:47 -0400

    add pagination library, add pagination to inspect-lists component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { CodemirrorModule } from '@ctrl/ngx-codemirror';
+import {NgxPaginationModule} from 'ngx-pagination';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -29,7 +30,8 @@ import { UploadComponent } from './upload/upload.component';
       AppRoutingModule,
       HttpClientModule,
       FormsModule,
-      CodemirrorModule
+      CodemirrorModule,
+      NgxPaginationModule
    ],
    providers: [],
    bootstrap: [
